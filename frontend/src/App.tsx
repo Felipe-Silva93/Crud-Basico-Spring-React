8c8023b0facc63d105402b02b2ac27fab5480b4d
felipe <felipesilva4127@gmail.com>
2022-11-17 15:13:39 -0300

    feat: Rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Form from "./pages/Form";
+import Listing from "./pages/Listing";
+
 
 function App() {
   return (
-    <NavBar/>
-);
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":Id" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
