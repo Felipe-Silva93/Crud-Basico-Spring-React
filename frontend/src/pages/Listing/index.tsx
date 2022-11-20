1f4b42051f102447ea9ed10ad583bbdee0330ff2
felipe <felipesilva4127@gmail.com>
2022-11-20 15:28:26 -0300

    feat: alterando tabela

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -21,71 +21,49 @@ const Listing: React.FC=()=>{
         loadListing()
 
 
-    })
+    },[])
 
 
 
     async function loadListing(){
 
-        const responde = await api.get('/listing')
+        const responde = await api.get('/crud')
         console.log(responde)
         setListings(responde.data)
     }
 
 
     return(
-        <div className="container">
-            <br/>
-            <h1>pagina de listagem</h1>
-            <br/>
+    <Table striped bordered hover>
+        <thead>
+            <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            </tr>
+        </thead>
+        <tbody>
             
-
-
-            <Table striped bordered hover className="text-center">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>E-mail</th>
-                    <th>Senha</th>
-                    <th>Status</th>
-                    <th>Açoes</th>
-                </tr>
-            </thead>
-            <tbody>
-
                 {
                     listings.map(listing=>(
 
-                        <tr key={listing.id}>
-                            <td>{listing.id}</td>
-                            <td>{listing.nome}</td>
-                            <td>{listing.email}</td>
-                            <td>{listing.senha}</td>
-                            <td>
-                                <Badge bg ={listing.status? "success":"warning"}>
-                                    {listing.status? "finalizada":"pendente"}
-                                </Badge>
-        
-                            </td>
-                            <td>
-                                <Button variant="warning">editar</Button>
-                                <Button variant="warning">Wa</Button>
-                                <Button variant="warning">rning</Button>
-                                <Button variant="warning">ng</Button>
-                            </td>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
                         </tr>
+                    
                     ))
                 }
-
+        </tbody>        
                 
-                
-            </tbody>
-            </Table>
-        
+            
+       
+      
+    </Table>
 
-
-        </div>
     );
 
 }
