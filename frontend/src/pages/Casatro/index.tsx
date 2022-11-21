84c99157e0959ba7f72440037763af24a8c67cf6
felipe <felipesilva4127@gmail.com>
2022-11-21 20:58:59 -0300

    feat: implementando um evento no botao de salvar

--- frontend/src/pages/Casatro/index.tsx
+++ frontend/src/pages/Casatro/index.tsx
@@ -1,21 +1,62 @@
 import { Badge, Button, Form, Table } from "react-bootstrap";
-import React,{useState, useEffect } from 'react';
+import React,{useState, useEffect, ChangeEvent } from 'react';
 import { useHistory } from "react-router-dom";
 
 import api from 'api'
 
 
+interface Iform{
+    
+    nome: string ;
+    email:string;
+    senha:number;
+    
 
+}
 
 
 const Cadastro: React.FC=() => {
     
+    const [showElement, setShowElement] = useState(false)
+  const showOrHide = () => setShowElement(true)
     const history = useHistory()
+
+    const[model, setModel] =useState<Iform>({
+
+    nome: '',
+    email:'',
+    senha:0
+
+    })
+
+    function updated(e: ChangeEvent<HTMLInputElement>){
+        
+        setModel({
+            ...model, //desistruturaçãi do que já tiver no model
+
+            [e.target.name]: e.target.value
+
+        })
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+
+        e.preventDefault()
+        const response = await api.post ('/crud/cadastrar',model)
+        console.log(response)
+        crud()
+    }
 
 
     function login(){
         history.push('/form')
     }
+
+    function crud(){
+        history.push('/listing')
+    }
+
+
     return(
         <div className="container">
             <br />
@@ -24,26 +65,42 @@ const Cadastro: React.FC=() => {
             <br />
             <div className="container">
             
-            <>
+            <Form onSubmit={onSubmit}>
+
                 <Form.Group className="mb-3">
                     <Form.Label>E-mail:</Form.Label>
-                    <Form.Control type="email" placeholder="email meu bom" />
+                    <Form.Control 
+                    type="text" name ="email" 
+                    onChange={(e:ChangeEvent<HTMLInputElement>) => updated(e)} 
+                    placeholder="email meu bom" />
                 </Form.Group>
+
                 <Form.Group className="mb-3">
                     <Form.Label>Nome:</Form.Label>
-                    <Form.Control type="nome" placeholder="seu lindo nome" />
+                    <Form.Control type="text" name ="nome" 
+                    onChange={(e:ChangeEvent<HTMLInputElement>) => updated(e)}
+                    placeholder="seu lindo nome" />
                 </Form.Group>
+
                 <Form.Group className="mb-3">
                     <Form.Label>senha:</Form.Label>
-                    <Form.Control type="email" placeholder="senha jhow" />
+                    <Form.Control type="number"name ="senha"
+                    onChange={(e:ChangeEvent<HTMLInputElement>) => updated(e)}
+                    placeholder="senha jhow" />
                 </Form.Group>
                 
                 <Form.Group className="mb-3">
                     <Form.Check type="checkbox" label="Can't check this" disabled />
                 </Form.Group>
-             </>
-                <Button type="submit"size="sm">Submit</Button>{' '}
-                <Button variant="dark "size="sm" onClick={login}>login</Button>            
+                <Button  type="submit"size="sm" >Salvar</Button>{' '}
+                <Button variant="dark "size="sm" onClick={login}>login</Button>
+             </Form>
+
+             <div>
+                <button onClick={showOrHide}>Clique em mim</button>
+                { showElement ? <p>Tô aqui</p> : null }
+            </div>
+                            
 
             </div>
 
